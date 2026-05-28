7d4d447fbdb64aa903b00a37816e3d93e109de6b
Daniel Sweeney <danielconnorsweeney@gmail.com>
2026-05-28 14:41:02 -0400

    conditional statements added

--- Labs/Lab-2/missionDecisionEngine.js
+++ Labs/Lab-2/missionDecisionEngine.js
@@ -4,3 +4,15 @@ let shield = true;
 let missionProgress = 40;
 let enemyNearby = true;
 const decision = document.querySelector("#decision");
+
+decision.textContent = "System Stable";
+
+if (health < 30 && enemyNearby){
+    decision.textContent = "CRITICAL ALERT: Immediate Evacuation Required";
+} else if (ammo < 5 || !shield){
+    decision.textContent = "Warning: Low Resources";
+} else if (1 < missionProgress < 70){
+    decision.textContent = "Mission In Progress";
+} else if (missionProgress > 70 && !enemyNearby){
+    decision.textContent = " Mission Complete Successfully";
+}
