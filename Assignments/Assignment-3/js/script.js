9a1611c990d530a287e5a922804f8fcfe22fd194
Daniel Sweeney <danielconnorsweeney@gmail.com>
2026-07-06 18:41:55 -0400

    add basic page structure

---
+++ Assignments/Assignment-3/js/script.js
@@ -0,0 +1,6 @@
+const studentInfo = document.querySelector("#student-info");
+
+const studentParagraph = document.createElement("p");
+studentParagraph.textContent = "Student Name: Daniel Sweeney |  Student ID: 200645157";
+
+studentInfo.appendChild(studentParagraph);
